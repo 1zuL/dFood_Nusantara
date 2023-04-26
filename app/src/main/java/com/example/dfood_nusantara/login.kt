b99c50e3c81cbbf5a2a27b4d762b3e26557159d5
Muhammad Farizul Imami Sudirman <ozuxekyosuke@gmail.com>
2023-04-26 10:21:59 +0700

    register
    gfh

--- app/src/main/java/com/example/dfood_nusantara/login.kt
+++ app/src/main/java/com/example/dfood_nusantara/login.kt
@@ -1,11 +1,21 @@
 package com.example.dfood_nusantara
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+
+        val myTextView = findViewById<TextView>(R.id.lppw)
+        myTextView.setOnClickListener {
+            val intent = Intent(this, register::class.java)
+            startActivity(intent)
+        }
+
     }
 }
